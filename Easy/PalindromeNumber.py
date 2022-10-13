8d1c5d3a09544ebaebbf4959bef3a27ade8939b3
ttrung2h <hoangtrungcongviec@gmail.com>
2022-10-13 19:39:04 +0700

    new problem

---
+++ Easy/PalindromeNumber.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        
+        x = str(x)
+        if x == x[::-1]:
+            return True
+        return False
+
+solution = Solution()
+print(solution.isPalindrome(10))
