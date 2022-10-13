dd67488d58f7b3c65eab5951faffd433f220e5cf
ttrung2h <hoangtrungcongviec@gmail.com>
2022-10-13 18:54:10 +0700

    Add new solution

--- Easy/RemoveDuplicatesfromSortedArray.py
+++ Easy/RemoveDuplicatesfromSortedArray.py
@@ -1,17 +1,7 @@
 from typing import List
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        for num in nums:
-            x = len(nums)
-            a = nums.count(num)
-            if nums.count(num) > 1:
-                for i in range(a - 1):
-                    nums.remove(num)
-
-        if nums.count(nums[len(nums) - 1]) > 1:
-            nums.remove(nums[len(nums) - 1])
-        
-        return nums,len(nums)
-
+        x = set(nums)
+        return list(x),len(x)
 solution = Solution()
 print(solution.removeDuplicates([1,1,1,1]))
